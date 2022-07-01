905e913640f76000aca61b3cc9461ac60ff20f81
kipmcl <107448071+kipmcl@users.noreply.github.com>
2022-07-01 11:57:53 -0400

    Update submission.js

--- submission.js
+++ submission.js
@@ -1,6 +1,16 @@
 const findSum = function(array) {
-  // your code here - don't forget to return a number!
-};
+ var array = [2,4,8];
+
+    var arrayTotal = array.length; 
+    var totalSum = 0;
+
+    for(var x = 0; x < arrayTotal; x++){
+        totalSum += array[x];
+    }
+    return(totalSum); 
+  }
+var sum = findSum();
+console.log(sum);
 
 const findFrequency = function(array) {
   // your code here - don't forget to return an object!
